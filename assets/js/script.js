b3a6d1a403aaf36701bd5e0740407d500153c603
jcg <jcg.cristel@gmail.com>
2022-06-14 22:56:14 -0400

    added date parsing, displaying data for five week forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,47 +6,79 @@ var APIkey = `ec45c559fdda3ac235725be56933003e`;
 
 // Variables
 var suggestedCities = [];
-var weather;
+var cityName;
 
 // convert kelvin to celsius
 var kelvinToCelsius = function(kelvin) {
     return (Math.round(kelvin - 273.15));
 }
 
+var getDateTime = function(offset) {
+    // get base time
+    var baseTime = luxon.DateTime.now().setZone('UTC');    
+
+    // get offset
+    var offset = weather.timezone_offset;
+    
+    // get local time of city
+    return baseTime.plus({seconds: offset});
+}
+
+var getDateTimeString = function(time) {
+    return time.toLocaleString({ weekday: 'long', month: 'long', day: '2-digit' });
+}
+
 // display current weather
 var displayCurrentWeather = function() {
     var current = weather.current;
     var iconUrl = `https://openweathermap.org/img/wn/${current.weather[0].icon}@2x.png`;
-   
+
+    // determine time
+    var time = getDateTime(weather.offset);
+    var timeString = getDateTimeString(time);
+
+    // determine temperature
+    var temp = kelvinToCelsius(weather.current.temp)
+    
+    $(`#current-date`).text(timeString);
+    $(`#current-city`).text(cityName);
     $(`#current-icon`).attr("src", iconUrl);
-    $(`#temp`).text(`${current.temp}\u00B0`);
+    $(`#temp`).text(`${temp}\u00B0`);
     $(`#humidity`).text(`${current.humidity}%`)
     $(`#wind_speed`).text(`${current.wind_speed} m/s`);
     $(`#uvi`).text(`${current.uvi}`)
 }
 
 var displayForecastedWeather = function() {
-    // var day = weather.daily;
+    var day = weather.daily;
 
     var forecastEl = $(`.forecast-weather`);
     forecastEl.empty();
 
-    for (var i = 1; i <= 5; i++) {  
-        console.log(i);      
+    for (var i = 1; i <= 5; i++) {
         var dayCardEl = $(`<div>`).addClass(`card day`);
-        
+
+        // determine day
+        var time = getDateTime(weather.timezone_offset);
+        time = time.plus({days: i}); // get the following day
+        var timeString = getDateTimeString(time);
+
+        // determine low and high temp in celsius
+        var max = kelvinToCelsius(day[i].temp.max);
+        var min = kelvinToCelsius(day[i].temp.min);
+
         // create date element
-        var dayDateEl = $(`<p>`).text('Date');
+        var dayDateEl = $(`<p>`).text(timeString);
         
         // create main info element
         var dayMainInfoEl = $('<div>').addClass(`forecast-main-info`);
-        var dayIcoEl = $(`<img>`).attr(`src`, `https://openweathermap.org/img/wn/10d.png`)
-        var dayTempEl = $(`<p>`).text("22");
-        dayMainInfoEl.append(dayIcoEl);
+        var dayIcoEl = $(`<img>`).attr(`src`, `https://openweathermap.org/img/wn/${day[i].weather[0].icon}.png`)
+        var dayTempEl = $(`<p>`).text(`${min}\u00B0/${max}\u00B0`);
+        dayMainInfoEl.append(dayIcoEl, dayTempEl);
 
         // create other info element
-        var dayWindEl = $(`<p>`).text("20 m/s");
-        var dayHumidityEl = $(`<p>`).text("66%");
+        var dayWindEl = $(`<p>`).text(`${day[i].wind_speed} m/s`);
+        var dayHumidityEl = $(`<p>`).text(`${day[i].humidity}%`);
 
         // display onto html document
         dayCardEl.append(dayDateEl, dayMainInfoEl, dayWindEl, dayHumidityEl);
@@ -64,9 +96,8 @@ var getWeather = function(lat, lon) {
     .then(response => response.json())
     .then(data => {
             weather = data;
-            weather.current.temp = kelvinToCelsius(data.current.temp);
             displayCurrentWeather();
-            // displayForecastedWeather();
+            displayForecastedWeather();
         });
 }
     
@@ -80,7 +111,8 @@ var getCity = function() {
         .then(data => {
             getWeather(data[0].lat, data[0].lon);
             savePrevCity(data[0].name, data[0].state, data[0].country);
-            
+            cityName = data[0].name;
+
             // clears input
             $("#city").val("");
 
@@ -119,7 +151,8 @@ var displaySuggestedCityList = function() {
 
 // pulls a list of cities depending on what is written in the input
 var getCityList = function() {
-var cityName = $("#city").val();
+cityName = $("#city").val();
+
 var geoCode = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=${limit}&appid=${APIkey}`;
 
     fetch(geoCode)
